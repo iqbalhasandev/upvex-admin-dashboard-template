ac66c912679724b75803bf6744f0c1fb6d164d03
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2022-05-29 16:19:47 +0600

    first commit

---
+++ dark/src/js/pages/email-summernote.init.js
@@ -0,0 +1,17 @@
+/*
+Template Name: Upvex - Responsive Bootstrap 4 Admin Dashboard
+Author: CoderThemes
+Version: 1.0.0
+Website: https://coderthemes.com/
+Contact: support@coderthemes.com
+File: Email Summernote init js
+*/
+
+$(document).ready(function(){
+    $('.summernote').summernote({
+        height: 230,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: false                 // set focus to editable area after initializing summernote
+    });
+});
